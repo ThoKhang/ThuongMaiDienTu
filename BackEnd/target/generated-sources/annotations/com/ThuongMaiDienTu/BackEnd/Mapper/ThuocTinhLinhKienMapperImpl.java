a245520db79a89158997222f7901bded64181186
HuyPham <phamminhhuy0901tk@gmail.com>
2026-06-03 11:22:34 +0700

    .

--- BackEnd/target/generated-sources/annotations/com/ThuongMaiDienTu/BackEnd/Mapper/ThuocTinhLinhKienMapperImpl.java
+++ BackEnd/target/generated-sources/annotations/com/ThuongMaiDienTu/BackEnd/Mapper/ThuocTinhLinhKienMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-03T09:48:46+0700",
+    date = "2026-06-03T11:11:02+0700",
     comments = "version: 1.6.3, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
 )
 @Component
